f440ae884fc0327dffc3957c3c9eaf19a3027e51
electriclantern <quewon.chin@gmail.com>
2019-01-14 00:16:33 +0900

    .
    .

---
+++ script.js
@@ -0,0 +1,8 @@
+function menu(menu) {
+  document.getElementById(menu).style.display = "block";
+  if (menu=='garden') {
+    document.getElementById('brewshop').style.display = "none";
+  } else {
+    document.getElementById('garden').style.display = "none";
+  }
+}
