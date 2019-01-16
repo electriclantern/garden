85b6b77c2b5d31e64a8a5ca4f39fa9f567ad2747
electriclantern <quewon.chin@gmail.com>
2019-01-16 23:22:50 +0900

    .

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function toggleTheme() {
 }
 
 function isOverflown(element) {
-    return element.scrollHeight+10 > element.clientHeight || element.scrollWidth > element.clientWidth;
+    return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
 }
 
 function menu(menu) {
